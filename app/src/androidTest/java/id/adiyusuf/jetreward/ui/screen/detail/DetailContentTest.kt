26dbd061d9586b92f895272b9722d639c148515c
DESKTOP-H9VG51M\Adi <jakarta>
2025-03-12 14:29:42 +0700

    refactor: UI Testing

---
+++ app/src/androidTest/java/id/adiyusuf/jetreward/ui/screen/detail/DetailContentTest.kt
@@ -0,0 +1,86 @@
+package id.adiyusuf.jetreward.ui.screen.detail
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assert
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsEnabled
+import androidx.compose.ui.test.assertIsNotEnabled
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.printToLog
+import id.adiyusuf.jetreward.R
+import id.adiyusuf.jetreward.model.OrderReward
+import id.adiyusuf.jetreward.model.Reward
+import id.adiyusuf.jetreward.onNodeWithStringId
+import id.adiyusuf.jetreward.ui.theme.JetRewardTheme
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class DetailContentTest {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    private val fakeOrderReward = OrderReward(
+        reward = Reward(4, R.drawable.reward_4, "Jaket Hoodie Dicoding", 7500),
+        count = 0
+    )
+
+    @Before
+    fun setUp() {
+        composeTestRule.setContent {
+            JetRewardTheme {
+                DetailContent(
+                    fakeOrderReward.reward.image,
+                    fakeOrderReward.reward.title,
+                    fakeOrderReward.reward.requiredPoint,
+                    fakeOrderReward.count,
+                    onBackClick = {},
+                    onAddToCart = {}
+                )
+            }
+        }
+        composeTestRule.onRoot().printToLog("currentLabelExists")
+    }
+
+    @Test
+    fun detailContent_isDisplayed() {
+        composeTestRule.onNodeWithText(fakeOrderReward.reward.title).assertIsDisplayed()
+        composeTestRule.onNodeWithText(
+            composeTestRule.activity.getString(
+                R.string.required_point,
+                fakeOrderReward.reward.requiredPoint
+            )
+        ).assertIsDisplayed()
+    }
+
+    @Test
+    fun increaseProduct_buttonEnabled(){
+        composeTestRule.onNodeWithContentDescription("Order Button").assertIsNotEnabled()
+        composeTestRule.onNodeWithStringId(R.string.plus_symbol).performClick()
+        composeTestRule.onNodeWithContentDescription("Order Button").assertIsEnabled()
+    }
+
+    @Test
+    fun increaseProduct_correctCounter(){
+        composeTestRule.onNodeWithStringId(R.string.plus_symbol).performClick().performClick()
+        composeTestRule.onNodeWithText("2").assertIsDisplayed()
+    }
+
+    @Test
+    fun increaseProduct_correctCounter_withTag(){
+        composeTestRule.onNodeWithStringId(R.string.plus_symbol).performClick().performClick()
+        composeTestRule.onNodeWithTag("count").assert(hasText("2"))
+    }
+
+    @Test
+    fun decreaseProduct_stillZero(){
+        composeTestRule.onNodeWithStringId(R.string.minus_symbol).performClick()
+        composeTestRule.onNodeWithTag("count").assert(hasText("0"))
+    }
+}
